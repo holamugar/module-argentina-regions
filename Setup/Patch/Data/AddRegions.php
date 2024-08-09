0df81a951a27f13ce4b5dc60ee9ab586d0568ae5
Damián Culotta <damianculotta@gmail.com>
2024-08-09 12:10:29 -0300

    Add compatibility with latest versions of Magento

--- Setup/Patch/Data/AddRegions.php
+++ Setup/Patch/Data/AddRegions.php
@@ -1,9 +1,7 @@
 <?php
 /**
  * Argentina Regions
- *
- * @category   Mugar
- * @package    Mugar_ArgentinaRegions
+ 
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
  * @author     Mugar (https://www.mugar.io/)
  */
--- registration.php
+++ registration.php
@@ -2,8 +2,6 @@
 /**
  * Argentina Regions
  *
- * @category   Mugar
- * @package    Mugar_ArgentinaRegions
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
  * @author     Mugar (https://www.mugar.io/)
  */
